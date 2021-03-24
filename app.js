8b3645ed1a7049b14ca94b0d8078e53d1777d561
Mohit Tiwari <55630005+themohittiwari@users.noreply.github.com>
2021-03-25 00:02:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+router.post("/edit/vendor",(req,res)=>{
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "1500011@np.balbharati.org", // generated ethereal user
+          pass: "charutiwari", // generated ethereal password
+        },
+    });
+        let info =  transporter.sendMail({
+            from: '"Cocentric.in" <foo@example.com>', // sender address
+            to: req.body.details.email, // list of receivers
+            subject: "One time password", // Subject line
+            text: `your otp for edit vendor details is ${req.body.details.otp}`, // plain text body
+          });
+})
+
+
+app.listen(5000, () => console.log("Server Running"));
