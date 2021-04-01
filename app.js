1e061322e22793d3d9d8577c39c218ddaf952304
Mohit Tiwari <55630005+themohittiwari@users.noreply.github.com>
2021-04-01 12:46:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use("/", router);
 
-router.post("/edit/vendor",(req,res)=>{
+
+app.post("/edit/vendor",(req,res)=>{
     let transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
         port: 465,
